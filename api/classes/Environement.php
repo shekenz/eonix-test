68d39ebdc3a96b4d3cfc547825d4149e568deef1
shekenz <2657910+shekenz@users.noreply.github.com>
2022-09-12 13:23:58 +0200

    Singleton wrapper to initiate Dotenv

---
+++ api/classes/Environement.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace API;
+
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
+use API\Logger;
+
+class Environement
+{
+    private static $instance;
+    private $logger;
+
+    private function __construct()
+    {
+        $this->logger = Logger::getInstance();
+
+        try
+        {
+            self::$instance = Dotenv::createImmutable(__DIR__.'/../../');
+            self::$instance->load();
+        }
+
+        catch(InvalidPathException $e)
+        {
+            $this->logger->emergency($e->getMessage());
+            $this->logger->info('Make sure you copied .env.example to .env and correctly edited it.');
+
+            // TODO return HTTP 500
+            die();
+        }
+    }
+    
+    public static function init(): void
+    {
+        if(null === self::$instance)
+        {
+            self::$instance = new Environement;
+        }
+    }
+}
