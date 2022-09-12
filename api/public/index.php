91db432adbcf0d5c0e8c32af9359e52ec56ef42e
shekenz <2657910+shekenz@users.noreply.github.com>
2022-09-12 22:54:04 +0200

    Routed /create

--- api/public/index.php
+++ api/public/index.php
@@ -9,7 +9,7 @@ $users = new Users;
 
 $routes = [
     ['GET', '/users', [$users, 'get'] ],
-    ['GET', '/user/create', function() { echo 'Creating new user'; } ],
+    ['GET', '/user/create', [$users, 'create'] ],
     ['GET', '/user/{id:[0-9a-f]{32}}', [$users, 'get'] ],
     ['GET', '/user/update/{id:\d+}', function($data) { echo 'Updating user with id '.$data['id']; } ],
     ['GET', '/user/delete/{id:\d+}', function($data) { echo 'Deleting user with id '.$data['id']; } ],
