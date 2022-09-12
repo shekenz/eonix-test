2bc0a5a62123e3e8ccf471c418a6950ddf689b7d
shekenz <2657910+shekenz@users.noreply.github.com>
2022-09-12 17:13:51 +0200

    Added callback tests

--- tests/unit/UsersTest.php
+++ tests/unit/UsersTest.php
@@ -17,6 +17,13 @@ class UsersTest extends TestCase
         $this->assertTrue(method_exists(Users::class, 'get'));
     }
 
+    public function testHasMethodGetCallback(): void
+    {
+        $this->assertTrue(method_exists(Users::class, 'getCallback'));
+        $reflexion = new ReflectionMethod(Users::class, 'getCallback');
+        $this->assertTrue($reflexion->isPrivate(), 'getCallback should be private');
+    }
+
     public function testGetReturnsArray(): void
     {
         $this->assertIsArray($this->users->get());
@@ -27,14 +34,35 @@ class UsersTest extends TestCase
         $this->assertTrue(method_exists(Users::class, 'create'));
     }
 
+    public function testHasMethodCreateCallback(): void
+    {
+        $this->assertTrue(method_exists(Users::class, 'createCallback'));
+        $reflexion = new ReflectionMethod(Users::class, 'createCallback');
+        $this->assertTrue($reflexion->isPrivate(), 'createCallback should be private');
+    }
+
     public function testHasMethodUpdate(): void
     {
-        $this->assertTrue(method_exists(Users::class, 'delete'));
+        $this->assertTrue(method_exists(Users::class, 'update'));
+    }
+
+    public function testHasMethodUpdateCallback(): void
+    {
+        $this->assertTrue(method_exists(Users::class, 'updateCallback'));
+        $reflexion = new ReflectionMethod(Users::class, 'updateCallback');
+        $this->assertTrue($reflexion->isPrivate(), 'updateCallback should be private');
     }
 
     public function testHasMethodDelete(): void
     {
         $this->assertTrue(method_exists(Users::class, 'delete'));
+    }
+
+    public function testHasMethodDeleteCallback(): void
+    {
+        $this->assertTrue(method_exists(Users::class, 'deleteCallback'));
+        $reflexion = new ReflectionMethod(Users::class, 'deleteCallback');
+        $this->assertTrue($reflexion->isPrivate(), 'deleteCallback should be private');
     }
 
     public function testIdNotFound(): void
