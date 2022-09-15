36c47bbd425b807d2272ce7776aaa7a4bd5de4c1
shekenz <2657910+shekenz@users.noreply.github.com>
2022-09-15 17:09:05 +0200

    PUT & PATCH methods

--- api/public/index.php
+++ api/public/index.php
@@ -19,10 +19,10 @@ $users = new Users(json_decode(file_get_contents('php://input'), true));
 $routes = [
     ['POST', '/users', [$users, 'get'] ],
     ['POST', '/users/', [$users, 'get'] ],
-    ['POST', '/user/create', [$users, 'create'] ],
-    ['POST', '/user/create/', [$users, 'create'] ],
+    ['PUT', '/user/create', [$users, 'create'] ],
+    ['PUT', '/user/create/', [$users, 'create'] ],
     ['POST', '/user/{id:[0-9a-f]{32}}', [$users, 'get'] ],
-    ['POST', '/user/update/{id:[0-9a-f]{32}}', [$users, 'update'] ],
+    ['PATCH', '/user/update/{id:[0-9a-f]{32}}', [$users, 'update'] ],
     ['DELETE', '/user/delete/{id:[0-9a-f]{32}}', [$users, 'delete'] ],
 ];
 
